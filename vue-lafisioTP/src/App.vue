325575bfbc39e62ed698404c1958500e636732ee
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2022-10-15 23:42:51 -0500

    Se crea proyecto en Vue con html y css de la vista home

---
+++ vue-lafisioTP/src/App.vue
@@ -0,0 +1,138 @@
+<template>
+    <div class="grid-container">
+      <header>
+      <div class="header-container">
+            <nav class="navigation">
+                    <figure>  
+                        <img src="./components/icons/Imagenes/Logo/Logo LAFISIOTP claro.jpg" alt="Logo" class="logo">
+                    </figure>
+                    <ul>
+                        <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Servicios</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Citas</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">LaFisiotp</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Tarifas</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Contáctanos</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Iniciar sesión</a></li>
+                    </ul>
+            </nav>
+        </div>
+    </header>
+    <section class="home">
+        <div class="main-section">
+            <figure>
+            <img src="./components/icons/Imagenes/imagen-home.jpg" alt="home" id="home-img">
+          </figure>
+          <div class="info">
+            <figure>
+              <img src="./components/icons/Imagenes/Logo/Logo LAFISIOTP oscuro new 2.jpg" alt="logo oscuro" class="logo" id="logo-dark">
+            </figure> 
+            <button class="btn">más información</button>
+          </div>
+        </div>
+    </section>
+    </div>
+
+    <!-- -----------Servicios---------- -->
+    <section class="services">
+        <div class="services-container">
+            <div class="service">
+                <h1>Rehabilitación Integral</h1>
+                <a href="#">Explora</a>
+            </div>
+            <div class="service">
+                <h1>Terapias de relajación</h1>
+                <a href="#">Explora</a>
+            </div>
+            <div class="service">
+                <h1>Vendaje de Kinesiotape</h1>
+                <a href="#">Explora</a>
+            </div>
+            <div class="service">
+                <h1>Acupuntura</h1>
+                <a href="#">Explora</a>
+            </div>
+            <div class="service">
+                <h1>Moxibustión</h1>
+                <a href="#">Explora</a>
+            </div>
+            <div class="service">
+                <h1>Reflexología Podal</h1>
+                <a href="#">Explora</a>
+            </div>
+            <div class="service">
+                <h1>Reflexología Auricular</h1>s
+                <a href="#">Explora</a>
+            </div>
+            </div>
+    </section>
+
+  <!-- -----------Explora---------- -->
+<section class="details">
+    <div class="details-container">
+        <figure>
+            <img src="#" alt="">
+        </figure>
+        <h1></h1>
+        <p></p>
+    </div>
+</section>
+
+
+    <!-- -----------Citas---------- -->
+    <section class="dates">
+        <div class="dates-container">
+
+        </div>
+    </section>
+    <!-- -----------LAFISIOTP-------- -->
+    <section class="aboutMe">
+        <div class="aboutMe-container">
+            <figure>
+                <img src="#" alt="LaFisio">
+            </figure>
+            <h2>IVONNE HERRERA BETANCOURTH</h2>
+            <p>Fisioterapeuta</p>
+            <p>Universidad Manuela Beltrán</p>
+        </div>
+    </section>
+    <!-- -----------Tarifas---------- -->
+    <section class="rates">
+        <div class="rates-container">
+            <h1>Tarifa primera cita</h1>
+            <p>$80.000</p>
+            <p>Duración: 1 hora y 20 minutos</p>
+
+        </div>
+    </section>
+
+    <!-- -----------IniciarSesion---------- -->
+    <section class="session">
+        <div class="session-container">
+
+        </div>
+    </section>
+
+    <!-- -----------Contáctanos---------- -->
+    <section class="contact">
+        <div class="contact-container">
+
+        </div>
+    </section>
+    <footer>
+
+
+    </footer>
+
+</template>
+
+<script setup>
+
+
+</script>
+
+
+
+<style scoped>
+
+</style>
