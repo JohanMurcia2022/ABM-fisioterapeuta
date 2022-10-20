cf1f0812cd238880ac90110023c11f12d32c035c
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2022-10-20 15:34:21 -0500

    Se adiciona CSS de la sesión home y servicios

--- vue-lafisioTP/src/App.vue
+++ vue-lafisioTP/src/App.vue
@@ -8,7 +8,7 @@
                     </figure>
                     <ul>
                         <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link">Servicios</a></li>
+                        <li class="nav-item"><a href="#services" class="nav-link">Servicios</a></li>
                         <li class="nav-item"><a href="#" class="nav-link">Citas</a></li>
                         <li class="nav-item"><a href="#" class="nav-link">LaFisiotp</a></li>
                         <li class="nav-item"><a href="#" class="nav-link">Tarifas</a></li>
@@ -28,42 +28,52 @@
               <img src="./components/icons/Imagenes/Logo/Logo LAFISIOTP oscuro new 2.jpg" alt="logo oscuro" class="logo" id="logo-dark">
             </figure> 
             <button class="btn">más información</button>
+
           </div>
         </div>
     </section>
     </div>
 
     <!-- -----------Servicios---------- -->
-    <section class="services">
-        <div class="services-container">
-            <div class="service">
-                <h1>Rehabilitación Integral</h1>
-                <a href="#">Explora</a>
-            </div>
-            <div class="service">
-                <h1>Terapias de relajación</h1>
-                <a href="#">Explora</a>
-            </div>
-            <div class="service">
-                <h1>Vendaje de Kinesiotape</h1>
-                <a href="#">Explora</a>
-            </div>
-            <div class="service">
-                <h1>Acupuntura</h1>
-                <a href="#">Explora</a>
-            </div>
-            <div class="service">
-                <h1>Moxibustión</h1>
-                <a href="#">Explora</a>
-            </div>
-            <div class="service">
-                <h1>Reflexología Podal</h1>
-                <a href="#">Explora</a>
-            </div>
-            <div class="service">
-                <h1>Reflexología Auricular</h1>s
-                <a href="#">Explora</a>
-            </div>
+    <section id="services">
+        <div class="container">
+            <h1 class="subtitle">Servicios</h1>
+           <div class="services-list">
+                <div>
+                    <img src="./components/icons/Imagenes/iconos rehabilitacion.jpg" alt="iconos" id="icons">
+                    <h1>Rehabilitación Integral</h1>
+                    <a href="#">Tu recuperación es nuestro compromiso</a>
+                </div>
+                <div>
+                    <img src="./components/icons/Imagenes/fisioterapia tratamiento.jpeg" alt="fisio" id="fisio">
+                    <h1>Terapias de relajación</h1>
+                    <a href="#">Disminuye frecuencia cardiaca, presión arterial, mejora la calidad de sueño</a>
+                </div>
+                <div>
+                    <img src="./components/icons/Imagenes/Kinesiotape.jpg" alt="kinesiotape">
+                    <h1>Vendaje de Kinesiotape</h1>
+                    <a href="#">Las tiras kinesiológicas alivian el dolor de las zonas afectadas por lesiones articulares o musculares, ofrecen mejor movilidad, disminuyen la inflamación y mejoran la circulación</a>
+                </div>
+                <div>
+                    <img src="./components/icons/Imagenes/acupuntura01.png" alt="acupuntura">
+                    <h1>Acupuntura</h1>
+                    <a href="#"> la acupuntura tiene una gran capacidad de elevar la energía vital, fortalecer el sistema inmunológico, favorecer la circulación de la sangre y mejorar en un alto porcentaje el estado de salud en general.</a>
+                </div>
+                <div>
+                    <h1>Moxibustión</h1>
+                    <a href="#">La moxibustión es un tratamiento terapéutico que proviene de la medicina tradicional China basada en la aplicación de calor en diferentes partes de la anatomía humana como método para estimular diferentes partes del cuerpo con el fin de curar enfermedades o dolencias.</a>
+                </div>
+                <div>
+                    <img src="./components/icons/Imagenes/reflexologia.jpeg" alt="reflexologia">
+                    <h1>Reflexología Podal</h1>
+                    <a href="#">Promueve la relajación y alivia el estrés. Reduce el dolor en general.</a>
+                </div>
+                <div>
+                    <h1>Reflexología Auricular</h1>
+                    <a href="#">La auriculoterapia es una técnica de medicina tradicional china que se deriva de la acupuntura, a través de la cual se estimulan puntos del pabellón externo de la oreja para diagnosticar y poder dar tratamiento a problemas de salud en otras zonas del cuerpo.</a>
+                </div>
+
+           </div>
             </div>
     </section>
 
@@ -120,7 +130,7 @@
         </div>
     </section>
     <footer>
-
+        <p></p>
 
     </footer>
 
